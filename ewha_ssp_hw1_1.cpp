79ed53218d8a92e4253ef6cb2131219c7ecdf0b4
sunju0123 <sunjuhwang01@gmail.com>
2016-03-27 17:59:01 +0900

    1215139 Hwang Sunju HomeWork1-1

--- ewha_ssp_hw1_1.cpp
+++ ewha_ssp_hw1_1.cpp
@@ -5,20 +5,35 @@
  * GitHub Setup Homework 
  * Instructor: Prof. HyungJune Lee
  *
- * Coded by Firstname Lastname
- * Mar xx, 2015
+ * Coded by Sunju Hwang
+ * Mar 27, 2016
  *
  */
 
 // Add #define pre-processor below
+#include "led_test.h"
 
+#define  FRONT_LED_PIN 10
+#define REAR_LED_PIN  9
+
+int light = 0;
+int lightvalue=-1;
  
 void setup() {
   //Add your code of Lab Assignment #1
-
+	pinMode(FRONT_LED_PIN,OUTPUT);
+    pinMode(REAR_LED_PIN,OUTPUT);
 }
 
 void loop() {
   //Add your code of Lab Assignment #1  
+	if(light>=255) lightvalue*=-1;
+	if(light<=0) lightvalue*=-1;
 
+	analogWrite(FRONT_LED_PIN,light);
+	analogWrite(REAR_LED_PIN,255-light);
+
+	light+=lightvalue;
+
+	delay(11);
 }
